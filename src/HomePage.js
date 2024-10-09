c8f6e418a6d3e8f708d338ba61bbef04162bbd55
brennas22 <brenna.sorkin@gmail.com>
2024-10-09 13:18:51 -0400

    center alignment

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,13 +4,18 @@ import './HomePage.css'; // Import your CSS file
 
 function HomePage() {
   return (
-    <div className="container"> {/* Add a container */}
+    <div className="container">
       <h1>Welcome to Our Wedding Website!</h1>
       <p>We're so excited to celebrate with you on [Date] at [Location].</p>
+
+      {/* Add both images */}
+      <img src="/mobile-image.png" alt="Mobile View" className="mobile-image" />
+      <img src="/desktop-image.png" alt="Desktop View" className="desktop-image" />
+
       <Link to="/rsvp" className="rsvp-link">RSVP Here</Link> {/* Add a class to the link */}
-      {/* Add more content as needed */}
     </div>
   );
 }
 
 export default HomePage;
+
