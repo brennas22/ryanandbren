c78b77fabf22a2d7283afaf6a15064dd64df99ec
brennas22 <brenna.sorkin@gmail.com>
2024-10-10 16:30:14 -0400

    update faq

--- src/FAQ.js
+++ src/FAQ.js
@@ -11,16 +11,7 @@ function FAQ() {
       <p>Our wedding will take place at Sarma, a restaurant in Somerville, MA (249 Pearl St, Somerville, MA 02145) on September 13th, 2025. The festivities will start promptly (well, promptly-ish) at 5 PM, and go until about 10:30 PM. There will be an afterparty for anyone who still wants to boogie at the Abbey, a bar in Cambridge.</p>
       <h3>What should I wear?</h3>
       <p>This is a FANCY wedding!! Please use it as an excuse to get dressed to the nines, but you can skip the tuxedo, a suit is fine. For folks in dresses, we would love if you wore tea (ankle) or full length dresses in <strong> colorful floral prints</strong>. Ryan would like it to be known that the wedding is definitely <em>not</em> Jimmy Buffet themed, but Brenna would like it to be known that it is definitely <em>is</em> Jimmy Buffet themed. Upscale Jimmy Buffet. Jimmy Buffet if you forced him to go to the Oscars.</p>
-      <h3>What should I get you as a gift?</h3>
-      <p>You reallllly don't need to get us a gift. Seriously, it's not necessary. That being said, we just bought a house and are kinda broke from that so if you insist on giving us money, we will ... spend it.</p>
-      <h3>Can I bring my kids?</h3>
-      <p>No.</p>
-      <h3>Can I bring my a plus 1?</h3>
-      <p>Your very special party code will let you know exactly who is invited, but we're inviting everyone's real plus ones. If you didn't get a plus one it's probably because we don't like your significant other and you should consider breaking up with them.</p>
-      <h3>I'm visiting from out of town, where should I stay?</h3>
-      <p>Blah blah hotel recommendations</p>
-      <h3>Is it open bar?</h3>
-      <p>Pls.</p>
+   
     </div>
   );
 }
