28c6a445ecd4ebc5276915da2431e2cff949082d
brennas22 <brenna.sorkin@gmail.com>
2024-12-16 14:04:43 -0500

    faqs

--- src/FAQ.js
+++ src/FAQ.js
@@ -13,10 +13,10 @@ function FAQ() {
       <p>Our wedding is on September 13th, 2025. The festivities will start promptly (well, promptly-ish) at 5 PM, and go until about 10:30 PM. We will then head to the afterparty.</p>
       <p>5-6 PM: Cocktail hour (yes, we're starting with cocktails!)</p>
       <p>6-6:30 PM: Ceremony</p>
-      <p>6:30 PM onward: Dinner and dancing</p>
+      <p>6:30-10:30 PM: Dinner and dancing</p>
+      <p>10:30 PM onward: Afterparty at the Abbey</p>
       <h3>What should I wear?</h3>
       <p>This is a FANCY wedding!! Please use it as an excuse to get dressed to the nines, but you can skip the tuxedo, a suit is fine. For folks in dresses, we would love if you wore ankle or full length dresses in <strong> colorful floral prints</strong>. Ryan would like it to be known that the wedding is definitely <em>not</em> Jimmy Buffet themed, but Brenna would like it to be known that it definitely <em>is</em> Jimmy Buffet themed. Upscale Jimmy Buffet. Jimmy Buffet if you forced him to go to the Oscars.</p>
-   
     </div>
   );
 }
