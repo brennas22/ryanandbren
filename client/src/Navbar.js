a078d88b1ca6a2c57647c36d8c93b6fd7acb15de
Ryan <ryan@Ryans-MacBook-Air.local>
2025-05-01 21:18:39 -0400

    npm builds plus testing

---
+++ client/src/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const closeMobileMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <Link 
+        to="/" 
+        className="navbar-logo"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        Ryan & Brenna
+      </Link> 
+      <div className="hamburger-menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      {isMenuOpen && (
+        <div
+          className={`overlay ${isMenuOpen ? 'open' : ''}`}
+          onClick={() => setIsMenuOpen(false)}
+        ></div>
+      )}
+      <div className={`navbar-links ${isMenuOpen ? 'open' : ''}`}>
+        <button className="close-button" onClick={() => setIsMenuOpen(false)}>
+          &times;
+        </button>
+        <Link to="/gifts" onClick={closeMobileMenu}>Gifts</Link>
+        <Link to="/faq-container" rel="noopener noreferrer">FAQs</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/gift-list.js
@@ -0,0 +1,33 @@
+// gift-list.js
+const GiftList = [
+    {
+      title: 'AI Admin Tool',
+      subtitle: 'Product Design @ Toast',
+      description: 'Designed a tool to allow finetuning of AI agents by non-engineers.',
+      image: '/admin-tool.png',
+      link: '/admin-tool',
+    },
+    {
+      title: 'Search & "The Donut"',
+      subtitle: 'Product Design @ Toast',
+      description: "Revamped the search experience as part of the Toast Web Operating System.",
+      image: '/search.png',
+      link: '/search',
+    },
+    {
+      title: 'Manager Close Out',
+      subtitle: 'Product Design @ Toast',
+      description: 'Completely redesigned the end-of-day experience on Point of Sale devices.',
+      image: '/manager-close-out.png',
+      link: '/manager-close-out',
+    },
+    {
+      title: 'Gifting Program',
+      subtitle: 'Product Design @ Drizly',
+      description: "Led design on Drizly's gifting program ($2 million impact).",
+      image: '/gifting.png',
+      link: '/gifting-program',
+    },
+  ];
+  
+  export default GiftList;
